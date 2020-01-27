d5862b34dc6f832d64bae166e6d1e693b0f2743d
Zach Brown <zachmb22@gmail.com>
2020-01-27 03:42:00 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,8 +1,13 @@
 # require modules here
 
-def load_library
-  # code goes here
-end
+def load_library(file_path)
+  library = YAML.load_file(file_path)
+  result = {"get_meaning" => {}, "get_emoticon" => {}}
+  library.each do |meaning, emoticons|
+    result["get_meaning"][emoticons[1]] = meaning
+    result["get_emoticon"][emoticons[0]] = emoticons[1]
+  end
+  result
 
 def get_japanese_emoticon
   # code goes here
