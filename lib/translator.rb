e3e9bd0ffa0798c3c04a0b2edacce0ec024f8772
Zach Brown <zachmb22@gmail.com>
2020-01-27 03:44:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,6 +15,8 @@ def get_japanese_emoticon(file_path = './lib/emoticons.yml', english_emoticon)
   library = load_library(file_path)
   if library["get_emoticon"].include?(english_emoticon) 
     library["get_emoticon"][english_emoticon]
+  else
+    "Sorry, that emoticon was not found"
   end
 end
 
